72aeaeff0912c31b575eecee50f92408324d399e
MertEkinci58 <67189482+MertEkinci58@users.noreply.github.com>
2021-03-06 01:36:10 +0300

    Update Main.java
    content updated from website

--- src/io/testinium/Main.java
+++ src/io/testinium/Main.java
@@ -3,6 +3,6 @@ package io.testinium;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from intelij!!");
+        System.out.println("Hello from website!!");
     }
 }
