74010440bb45e7171ec529596ee5fd06a5779ed9
mertekinci58 <mert.ekinci.1995@gmail.com>
2021-03-06 01:22:10 +0300

    hello sending from intelij

--- src/io/testinium/Main.java
+++ src/io/testinium/Main.java
@@ -3,6 +3,6 @@ package io.testinium;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello changed!!");
+        System.out.println("Hello from intelij!!");
     }
 }
